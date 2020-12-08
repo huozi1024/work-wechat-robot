94e75a147c5d203766094735a224419911039ee0
lio990527 <lio990527@163.com>
2020-12-08 18:23:46 +0800

    add monolog formatter

--- src/Monolog/Handler/RobotHandler.php
+++ src/Monolog/Handler/RobotHandler.php
@@ -2,6 +2,7 @@
 namespace Huozi\WorkWechat\Monolog\Handler;
 
 use Huozi\WorkWechat\WorkWechatRobot;
+use Huozi\WorkWechat\Monolog\Formatter\MarkdownFormatter;
 
 class RobotHandler extends \Monolog\Handler\AbstractProcessingHandler
 {
@@ -13,19 +14,17 @@ class RobotHandler extends \Monolog\Handler\AbstractProcessingHandler
     public function __construct($robotKey, $msgType = 'text', $level = \Monolog\Logger::ALERT, $bubble = true)
     {
         parent::__construct($level, $bubble);
-        $this->robot[$robotKey] = $this->robot[$robotKey] ?? new WorkWechatRobot($robotKey);
-        $this->msgType[$robotKey] = $msgType;
+        $this->robot = new WorkWechatRobot($robotKey);
     }
 
     protected function write(array $record)
     {
-        array_walk($this->robot, function ($robot, $robotKey) use ($record) {
-            if (method_exists($this, $this->msgType[$robotKey] . 'Msg')) {
-                $this->{$this->msgType[$robotKey] . 'Msg'}($robot, $record);
-            } else {
-                $robot->textMsg($robot, $record);
-            }
-        });
+        $formatter = $this->getFormatter();
+        if ($formatter instanceof MarkdownFormatter) {
+            $this->markdownMsg($record);
+        } else {
+            $this->textMsg($record);
+        }
     }
 
     /**
@@ -33,13 +32,12 @@ class RobotHandler extends \Monolog\Handler\AbstractProcessingHandler
      * @param WorkWechatRobot $robot
      * @param array $record
      */
-    protected function textMsg($robot, array $record)
+    protected function textMsg(array $record)
     {
-        $content = $record['channel'] . '.' . $record['level_name'] . PHP_EOL . $record['message'] . PHP_EOL . json_encode($record['context'], JSON_UNESCAPED_UNICODE);
-        if (isset($record['extra']['url'])) {
-            $content .= PHP_EOL . "from: {$record['extra']['url']}";
+        if (strlen($record['formatted']) > 5120) {
+            $record['formatted'] = substr($record['formatted'], 0, 5117) . '...';
         }
-        $robot->text($content);
+        $this->robot->text($record['formatted']);
     }
 
     /**
@@ -47,15 +45,8 @@ class RobotHandler extends \Monolog\Handler\AbstractProcessingHandler
      * @param WorkWechatRobot $robot
      * @param array $record
      */
-    protected function markdownMsg($robot, array $record)
+    protected function markdownMsg(array $record)
     {
-        $record['context'] = json_encode($record['context'], JSON_UNESCAPED_UNICODE);
-        $content = $record['channel'] . '.' . $record['level_name'] . PHP_EOL;
-        $content .= ">message:`{$record['message']}`" . PHP_EOL;
-        $content .= ">context:`{$record['context']}`" . PHP_EOL;
-        if (isset($record['extra']['url'])) {
-            $content .= ">url:`{$record['extra']['url']}`";
-        }
-        $robot->markdown($content);
+        $this->robot->markdown($record['formatted']);
     }
 }
